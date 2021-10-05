dacfe514205359aae5fbea265deab81a5c9f52bd
TylerBuese <36856818+TylerBuese@users.noreply.github.com>
2021-10-05 08:02:47 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,86 +1,3 @@
-// #include "raylib.h"
-// #include <vector>
-// #include <string>
-
-// void writeLetter(const char *letter, float posx, float posy, int fontSize);
-
-// struct Letter
-// {
-//     float lposX[1000]{};
-//     float lposY[1000]{};
-//     char letters[1000]{"\0"};
-// };
-// int main()
-// {
-//     const int windowWidth{1280};
-//     const int windowHeight{720};
-//     const char title[50] = "Hello, world!";
-//     int i = 0;
-
-//     InitWindow(windowWidth, windowHeight, title);
-//     std::vector<char> items;
-//     int letterCount{0};
-//     int posX{20};
-//     int posY{20};
-
-//     while (!WindowShouldClose())
-//     {
-//         BeginDrawing();
-//         DrawRectangle(10, 10, windowWidth - 20, windowHeight - 20, RED);
-//         Letter letter;
-//         if (GetKeyPressed() > 0)
-//         {
-//             int key = GetCharPressed();
-//             letterCount++;
-//             items.push_back((char)key);
-//             if (posX >= windowWidth - 20)
-//             {
-//                 posY += 20;
-//                 posX = 20;
-//             }
-
-//             letter.letters[letterCount] = (char)key;
-//         }
-
-//         DrawText(std::to_string(letterCount).c_str(), 500, 500, 50, BLUE);
-//         DrawText(&items[0], 600, 500, 50, BLUE);
-//         for (int i = 0; i < items.size(); i++)
-//         {
-//             posX = 20;
-//             posY = 20;
-//             writeLetter(&items[0], posX, posY, 50);
-//             posX += 20;
-//         }
-//         //writeLetter(&items[0], posX, posY, 50);
-
-//         //DrawText(&name[letterCount], posX, posY, 50, BLUE);
-
-//         //writeLetter(&name[letterCount], posX, posY, 50);
-
-//         ClearBackground(BLACK);
-//         EndDrawing();
-//     }
-//     CloseWindow();
-
-//     return 0;
-// }
-
-// void writeLetter(const char *letter, float posx, float posy, int fontSize)
-// {
-//     DrawText(letter, posx, posy, fontSize, BLUE);
-// }
-
-/*******************************************************************************************
-*
-*   raylib [text] example - Input Box
-*
-*   This example has been created using raylib 3.5 (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-*   Copyright (c) 2017 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "raylib.h"
 #include "string"
 #include "vector"
@@ -98,15 +15,18 @@ int main(void)
 
     char name[MAX_INPUT_CHARS + 1] = "\0"; // NOTE: One extra space required for null terminator char '\0'
     char prevLine[1024] {*name};
+    int sizeOfLines {32};
+    bool lines[sizeOfLines] {false};
     int letterCount = 0;
     int keysPressed {0};
+    int index {0};
 
     Rectangle textBox = {10, 10, screenWidth - 20, screenHeight - 20};
     bool mouseOnText = false;
 
     int framesCounter = 0;
 
-    SetTargetFPS(10); // Set our game to run at 10 frames-per-second
+    SetTargetFPS(30); // Set our game to run at 10 frames-per-second
 
     float posX {20};
     float posY {20};
@@ -154,6 +74,8 @@ int main(void)
             key = GetCharPressed(); // Check next character in the queue
         }
 
+        
+
         // Get char pressed (unicode character) on the queue
 
         if (IsKeyPressed(KEY_BACKSPACE))
@@ -184,9 +106,9 @@ int main(void)
             DrawRectangleLines(textBox.x, textBox.y, textBox.width, textBox.height, DARKGRAY);
         
         DrawText(name, 40, 670, 40, MAROON);
-        if (IsKeyDown(KEY_ENTER))
+        if (IsKeyPressed(KEY_ENTER))
         {
-            lineOne = true;
+            lines[index] = true;
             for (int i = 0; i < keysPressed; i++)
             {
                 prevLine[i] += name[i];
@@ -198,13 +120,17 @@ int main(void)
             }
 
             keysPressed = 0;
+            letterCount = 0;
+            
             // DrawText(&prevLine[0], posX, posY + 40, 40, MAROON);   
         }
 
-        if (lineOne)
+        if (lines[index])
         {
             DrawText(prevLine, screenWidth / 64, posY, 40, MAROON);
         }
+
+        DrawText(std::to_string(index).c_str(), screenWidth / 64, posY, 40, MAROON);
         
         
 
@@ -246,3 +172,5 @@ bool IsAnyKeyPressed()
 
     return keyPressed;
 }
+
+
