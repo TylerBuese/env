4edd941be9b35bfa3df4c93d059e79269c597c7f
TylerBuese <36856818+TylerBuese@users.noreply.github.com>
2021-10-07 15:50:51 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,16 +14,17 @@ int main(void)
     InitWindow(screenWidth, screenHeight, "raylib [text] example - input box");
 
     char name[MAX_INPUT_CHARS + 1] = "\0"; // NOTE: One extra space required for null terminator char '\0'
-    char prevLine[1024] {*name};
-    char printedLines[1024][32];
-    int sizeOfLines {32};
-    bool lines[sizeOfLines] {false};
+    char prevLine[1024]{*name};
+    char printedLines[32][1024];
+    int sizeOfLines{32};
+    bool lines[sizeOfLines]{false};
     int letterCount = 0;
-    int keysPressed {0};
-    int index {0};
-    int oldItems {0};
-    int lineItem {0};
-
+    int keysPressed{0};
+    int index{0};
+    int oldItems{0};
+    bool triggered{false};
+    int x = 0;
+    int y = 0;
 
     Rectangle textBox = {10, 10, screenWidth - 20, screenHeight - 20};
     bool mouseOnText = false;
@@ -32,9 +33,9 @@ int main(void)
 
     SetTargetFPS(30); // Set our game to run at 10 frames-per-second
 
-    float posX {20};
-    float posY {20};
-    bool lineOne {false};
+    float posX{20};
+    float posY{20};
+    bool lineOne{false};
     //--------------------------------------------------------------------------------------
 
     // Main game loop
@@ -72,8 +73,6 @@ int main(void)
             key = GetCharPressed(); // Check next character in the queue
         }
 
-        
-
         // Get char pressed (unicode character) on the queue
 
         if (IsKeyPressed(KEY_BACKSPACE))
@@ -102,19 +101,16 @@ int main(void)
             DrawRectangleLines(textBox.x, textBox.y, textBox.width, textBox.height, RED);
         else
             DrawRectangleLines(textBox.x, textBox.y, textBox.width, textBox.height, DARKGRAY);
-        
+
         DrawText(name, 40, 670, 40, MAROON);
         if (IsKeyPressed(KEY_ENTER))
         {
-            lines[index] = true;
+            triggered = true;
             //adds all items in name to prevLine
             for (int i = 0; i < keysPressed; i++)
             {
-                prevLine[i] += name[i];
-                printedLines[i][0] += name[i]; 
+                printedLines[index][i] += name[i];
             }
-            printedLines[keysPressed + 1][0] = '\0';
-
 
             //reset terminal prompt
             for (int i = 0; i < keysPressed; i++)
@@ -124,26 +120,22 @@ int main(void)
             oldItems = keysPressed;
             keysPressed = 0;
             letterCount = 0;
-            
-            // DrawText(&prevLine[0], posX, posY + 40, 40, MAROON);   
+            index++;
+            y += 25;
         }
 
-        
-        int j = 0;
-        //DrawText(&prevLine[0], screenWidth / 64, posY, 40, MAROON);
-        for (int i = 0; i < oldItems + 1; i++)
+        if (triggered)
         {
-            DrawText(&printedLines[i][0], screenWidth / 64 + j, posY, 40, MAROON);
-            j += 25;
+            //DrawText(&prevLine[0], screenWidth / 64, posY, 40, MAROON);
+            
+            for (int i = 0; i < index; i++)
+            {
+                DrawText(&printedLines[i][0], screenWidth / 64, y), 40, MAROON);
+            }
+
         }
-                                 
-        
-
-
-
-        //DrawText(std::to_string(index).c_str(), screenWidth / 64, posY, 40, MAROON);
-        
-        
+        //DrawText(&printedLines[index][0], screenWidth / 64 + x, y, 40, MAROON);
+        DrawText(std::to_string(index).c_str(), 600, posY, 40, MAROON);
 
         //const char prevLines[] = {'A', 'B', 'C'}; //gwhen you call the array, it gives you everything up to the null term. i.e., prevLines[1] gives you b,c -not a
         if (mouseOnText)
@@ -183,5 +175,3 @@ bool IsAnyKeyPressed()
 
     return keyPressed;
 }
-
-
